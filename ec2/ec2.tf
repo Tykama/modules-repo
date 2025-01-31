8123e136e198223859e96a13b7d739da61f667e1
chieftonyit <chieftonyit1985@gmail.com>
2025-01-30 21:56:31 -0600

    feat: Fixed checkov test error

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,6 +1,17 @@
 resource "aws_instance" "test" {
   ami           = "ami-0c614dee691cbbf37"
   instance_type = var.inst_type
+  monitoring = true
+  ebs_optimized = true
+
+  root_block_device {
+    encrypted     = true
+  }
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
 
   tags = {
     Name      = var.name
